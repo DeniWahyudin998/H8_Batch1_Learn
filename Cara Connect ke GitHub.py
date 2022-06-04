e3093748a9cc631e03bf381130132459a4533899
unknown <deniwahyudin998@gmail.com>
2022-06-04 13:46:24 +0700

    Initial Commit add Sesi 1

---
+++ Cara Connect ke GitHub.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  4 11:43:28 2022
+
+@author: 081796
+"""
+
+git init
+git config --global user.email deniwahyudin998@gmail.com
+git config --list
+git remote add origin https://github.com/DeniWahyudin998/H8_Batch1_Learn.git
+##ada di Web …or create a new repository on the command line
+git status
